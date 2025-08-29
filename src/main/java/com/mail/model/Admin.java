896ada7b1c6bcd75fee1dcf3b33873efccc8fa47
Eddy Arriaga <eddyearriaga@gmail.com>
2025-08-29 15:22:41 -0500

    model-progress-4

---
+++ src/main/java/com/mail/model/Admin.java
@@ -0,0 +1,7 @@
+package com.mail.model;
+
+public class Admin extends User {
+    public Admin(String name, String emailAddress, String password) {
+        super(name, emailAddress, password);
+    }
+}
---
+++ src/main/java/com/mail/model/NormalUser.java
@@ -0,0 +1,7 @@
+package com.mail.model;
+
+public class NormalUser extends User {
+    public NormalUser(String name, String emailAddress, String password) {
+        super(name, emailAddress, password);
+    }
+}
